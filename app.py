e2f331fc93a22918369ea746fcedc06416357bbe
Gina Kleinsteinberg <ginklein@olburm.noho.st>
2022-03-30 13:15:13 +0200

    Changed req

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
 import helpers as hp
 st.set_page_config(layout = "wide")
